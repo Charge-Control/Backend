03189e902562b109ea786aba9e9436f1b304671b
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-03-15 11:42:35 +0900

    [feat] 프로젝트 기본 셋팅 및 템플릿 셋팅

---
+++ ChargeControlBackend/src/main/java/com/ChargeControl/www/Backend/ChargeControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ChargeControl.www.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeControlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeControlBackendApplication.class, args);
+	}
+
+}
